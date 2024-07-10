b8d184a3e39d47fa3150bfb03a7db564b2089f03
aquilessaltoasucama <aquilessaltoasucama@gmail.com>
2024-07-10 13:33:07 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,18 @@ import { TwitterFollowCard } from "./TwitterFollowCard";
 export function App() {
   return (
     <section className="App">
-      <TwitterFollowCard userName="carrlox_ann" name="CarrloX" isFollowing={true}/>
-      <TwitterFollowCard userName="paimonaUwU" name="Paimon" isFollowing={false}/>
+      <TwitterFollowCard
+        isFollowing={true}
+        userName="carrlox_ann">
+        CarrloX
+      </TwitterFollowCard>
+
+      <TwitterFollowCard
+        userName="paimonaUwU"
+        children={"paimon"}
+        isFollowing={false}>
+        <strong>Paimon</strong>
+      </TwitterFollowCard>
     </section>
   );
-}
+} 
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,25 +1,31 @@
-export function TwitterFollowCard({userName, name,isFollowing}) {
-    console.log(isFollowing)
-    
-    return (
-        <article className="tw-followcard">
-        <header className="tw-followcard-header">
-          <img
-            className="tw-followcard-avatar"
-            alt="mi foto de twitter : P"
-            src={`https://unavatar.io/${userName}`}
-          />
-          <div className="tw-followcard-info">
-            <strong className="tw-followcard-infoUser">{name}</strong>
-            <span className="tw-followcard-infoUserName">@{userName}</span>
-          </div>
-        </header>
-  
-        <aside>
-          <button className="tw-followcard-button">
-              seguir
-          </button>
-        </aside>
-      </article>
-    )
+export function TwitterFollowCard({
+  children,
+  userName = "unknown",
+  isFollowing,
+}) {
+  const text = isFollowing ? "siguiendo" : "seguir";
+
+  const buttonClassName = isFollowing
+    ? "tw-followcard-button is-following"
+    : "tw-followcard-button";
+
+  return (
+    <article className="tw-followcard">
+      <header className="tw-followcard-header">
+        <img
+          className="tw-followcard-avatar"
+          alt="mi foto de twitter : P"
+          src={`https://unavatar.io/${userName}`}
+        />
+        <div className="tw-followcard-info">
+          <strong className="tw-followcard-infoUser">{children}</strong>
+          <span className="tw-followcard-infoUserName">@{userName}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className={buttonClassName}>{text}</button>
+      </aside>
+    </article>
+  );
 }
